ad7eb74cf1441a7e18f5cefde0b17fbbfe63a8a6
DionnaLorayna <dionna.lorayna.s@southlandcollege.edu.ph>
2023-03-03 13:56:07 +0800

    revised file content

--- script.js
+++ script.js
@@ -75,20 +75,6 @@ function initMap() {
 window.initMap = initMap;
 
 
-//slide images
-var myIndex = 0;
-	carousel();
 
-	function carousel() {
-	  var i;
-	  var x = document.getElementsByClassName("mySlideImages");
-	  for (i = 0; i < x.length; i++) {
-	    x[i].style.display = "none";
-	  }
-	  myIndex++;
-	  if (myIndex > x.length) {myIndex = 1}
-	  x[myIndex-1].style.display = "block";
-	  setTimeout(carousel, 3500);
-	}
 
  
